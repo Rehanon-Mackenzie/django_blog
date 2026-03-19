05b8d996c6707eeb5a09d8941acd88d25d59440c
Rehanon-Mackenzie <fairywings38@hotmail.com>
2026-03-19 09:57:35 +0000

    add post fixtures

--- blog/models.py
+++ blog/models.py
@@ -14,7 +14,7 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
-    updated = models.DateTimeField(auto_now=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     class Meta:
         ordering = ["-created_on"]
