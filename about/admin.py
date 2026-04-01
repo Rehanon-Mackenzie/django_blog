36f7b1a800a1fa37517d1f44b5e44517e61eb839
Rehanon-Mackenzie <fairywings38@hotmail.com>
2026-04-01 18:27:37 +0100

    add docstrings to files

--- about/admin.py
+++ about/admin.py
@@ -5,6 +5,8 @@ from django_summernote.admin import SummernoteModelAdmin
 # Register your models here.
 
 @admin.register(About)
+
+
 class AboutAdmin(SummernoteModelAdmin):
     summernote_fields = ('content',)
 
--- about/models.py
+++ about/models.py
@@ -3,7 +3,11 @@ from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
+
 class About(models.Model):
+    """
+    Stores a single about me text.
+    """
     title = models.CharField(max_length=200)
     profile_image = CloudinaryField('image', default='placeholder')
     updated_on = models.DateTimeField(auto_now=True)
@@ -13,11 +17,16 @@ class About(models.Model):
 def __str__(self):
     return self.title
 
+
 class CollaborateRequest(models.Model):
+    """
+    Stores a  single collaboration request message
+    """
     name = models.CharField(max_length=200)
     email = models.EmailField()
     message = models.TextField()
     read = models.BooleanField(default=False)
 
+
 def __str__(self):
-        return f"Collaboration request from {self.name}"
+    return f"Collaboration request from {self.name}"
